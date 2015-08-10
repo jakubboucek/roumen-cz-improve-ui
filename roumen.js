b0b1edba59632a3072638b0a4acde72d20023aa9
Jakub Bouček <pan@jakubboucek.cz>
2015-08-10 08:44:14 +0200

    Clean code, fix gif in maso.

--- roumen.js
+++ roumen.js
@@ -2,12 +2,13 @@ if(/(rouming|maso)Show\.php/.test(location.href)) {
 	var olderButton = document.querySelector('.roumingButton a[title="Starší obrázek"],.masoButton a[title="Starší obrázek"]');
 	var targetA = document.querySelector('td[height="600"] a');
 	targetA.href = olderButton.href;
-	var targetImg = targetA.querySelector('img');
-	var wh = document.documentElement.clientHeight;
-	targetImg.style.maxHeight = 'calc(100vh - ' + targetImg.offsetTop + 'px)';
+	var targetImg = document.querySelector('td[height="600"] a img');
+	targetImg.style.maxHeight = 'calc(100vh - ' + targetImg.y + 'px)';
+	targetImg.style.maxWidth = 'calc(100vw - ' + targetImg.x + 'px)';
+
 }
-if(location.href.indexOf('roumingGIF.php') > 1) {
+if(/(rouming|maso)GIF\.php/.test(location.href)) {
 	var olderButton = document.querySelector('.roumingButton a[title="Starší GIF"]');
-	var targetA = document.querySelector('td.roumingForumMessage[align="center"] a');
+	var targetA = document.querySelector('td.roumingForumMessage[align="center"] a,td.masoForumMessage[align="center"] a');
 	targetA.href = olderButton.href;
 }
