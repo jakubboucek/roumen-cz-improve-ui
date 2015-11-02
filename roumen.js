7a61b8f68bb372615077b88a133a3a6f3a06edd1
Jakub Bouček <pan@jakubboucek.cz>
2015-11-02 05:15:23 +0100

    Add keyboard browsing between arts (left/right arrow) and like/unlike by "L" button.

--- roumen.js
+++ roumen.js
@@ -12,3 +12,23 @@ if(/(rouming|maso)GIF\.php/.test(location.href)) {
 	var targetA = document.querySelector('td.roumingForumMessage[align="center"] a,td.masoForumMessage[align="center"] a');
 	targetA.href = olderButton.href;
 }
+
+function arrowHandler(event) {
+	var button;
+	if(event.keyCode == 39) {
+		button = document.querySelector('.roumingButton a[title="Starší obrázek"],.masoButton a[title="Starší obrázek"],.roumingButton a[title="Starší GIF"]');
+	}
+	else if(event.keyCode == 37) {
+		button = document.querySelector('.roumingButton a[title="Novější obrázek"],.masoButton a[title="Novější obrázek"],.roumingButton a[title="Novější GIF"]');
+	}
+	else if(event.keyCode == 76) {
+		button = document.querySelector('.roumingButton a[title="Tento obrázek se mi líbí"],.masoButton a[title="Tento obrázek se mi líbí"],.roumingForumTitle a[title="Tento GIF je super!"],.masoForumTitle a[title="Tento GIF je super!"]');
+	}
+	if(button) {
+		button.className += " activated";
+		window.setTimeout(function(){button.className=button.className.replace(/(\s|^)activated(\s|$)/,' ')}, 300);
+		button.click();
+	}
+}
+
+window.addEventListener('keydown', arrowHandler, true);
