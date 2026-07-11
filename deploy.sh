72ccd1f1a657d29f44648f3cd29ffcc371fb43a1
Jakub Bouček <pan@jakubboucek.cz>
2026-07-11 22:48:17 +0200

    deploy: small fixes

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 RELEASE_FILE="$DIR/dist/release.zip"
 
 echo -n "Preparing dist... "
-rm -rf $DIR/dist 2> /dev/null || true
+rm -rf "$DIR/dist" 2> /dev/null || true
 echo "OK"
 
 echo "Creating ZIP release... "
-mkdir -p $DIR/dist
-find $DIR -type f -depth 1 -not -name '.*' -not -name '*.sh' -not -name '*.md' -not -name 'LICENSE' | zip -j $RELEASE_FILE -@
+mkdir -p "$DIR/dist"
+find "$DIR" -maxdepth 1 -type f -not -name '.*' -not -name '*.sh' -not -name '*.md' -not -name 'LICENSE' | zip -j "$RELEASE_FILE" -@
 echo
 echo "Release prepared to $RELEASE_FILE"
